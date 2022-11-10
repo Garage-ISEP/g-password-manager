a4cfbd9f413bbfd5dc45fd0f5253f523f9a92d1b
Totodore <prevottheodore@gmail.com>
2022-11-10 11:45:12 +0100

    Chore: Base commit

---
+++ app/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import "./app.scss";
+	import Login from "./lib/login.svelte";
+	import Main from "./lib/main.svelte";
+
+	let user = null;
+	let token = null;
+</script>
+
+<main>
+	{#if !user}
+		<Login />
+	{:else}
+		<Main />
+	{/if}
+</main>
---
+++ app/src/lib/main.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	
+</script>
+
+<header>
+	<h1>Garage Vault</h1>
+	<img src="./garage.png" alt="Garage logo" />
+</header>
